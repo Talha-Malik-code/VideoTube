15cafd45f7c69bfc4d80723ee70850ad47d8b205
Talha Malik <t4talhawork@gmail.com>
2025-08-04 07:45:47 +0500

    Shift from npm to pnpm and Basic Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Example from "./component/Example";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Layout from "./component/Layout";
 
 function App() {
   return (
-    <>
-      <Example />
-      <ToastContainer position="top-right" autoClose={3000} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <></>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/component/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Example from "./Example";
+
+const Layout = () => {
+  return (
+    <div className="">
+      <Example />
+    </div>
+  );
+};
+
+export default Layout;
